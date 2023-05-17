392112ddffac3a28d7dafacf04e61808d65b0096
timbo84 <timber110406@gmail.com>
2023-05-17 14:26:20 -0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import { Provider } from 'next-auth/client';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
+
+export default MyApp;
